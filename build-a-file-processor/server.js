cca4dc459419569988bdbfd72c24af7259e5de6b
Marisol Quevedo <151071790+solposer@users.noreply.github.com>
2026-09-19 00:42:06 +0000

    primera clase

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,1 +1,30 @@
 // Starter file — add your code here
+const fs = require('fs');
+console.log(fs);
+
+const data = fs.readFileSync("assets/poem.txt", { encoding: "utf8" });
+console.log(data);
+
+fs.readFile("assets/poem.txt", { encoding: "utf8" }, (err, data) => {
+  console.log(data);
+});
+
+const fsPromises = require("fs/promises");
+
+async function main() {
+  const data = await fsPromises.readFile("assets/poem.txt", {
+    encoding: "utf8",
+  });
+  console.log(data);
+}
+
+main();
+
+fs.writeFileSync("assets/output.txt", "Hello, freeCodeCamp!");
+fs.appendFileSync("assets/output.txt", "\nSecond line");
+
+const exists = fs.existsSync("assets/output.txt");
+console.log(exists); // true or false
+
+const entries = fs.readdirSync("assets");
+console.log(entries); // [ 'output.txt', 'poem.txt' ]
